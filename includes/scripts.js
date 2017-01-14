a88f5a7440df932c6526325131ab755b59261460
Amit Papushe Shely <papushe9@gmail.com>
2017-01-14 19:07:54 +0200

    First upload

---
+++ includes/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	// data: JSON.stringify({ "Name": userName, "Email" : email });
+	//
+	// $.ajax({
+	// url: "http://134.86.158.192/CodingTest/home/SignUp",
+	// type: "POST",
+	// data: {userNameData JSON.stringify(jsonObjects)},
+	// dataType: "json",
+	// beforeSend: function(x) {
+	// 	if (x && x.overrideMimeType) {
+	// 	x.overrideMimeType("application/j-son;charset=UTF-8");
+	// 	}
+	// },
+	// success: function(result) {
+	// 	import grails.converters.JSON;
+	// 	List userNameData = JSON.parse(params.userNameData);
+	// 	println "Hello " + userNameData[0].userName + ", you are signed up;)";
+	// }
+	// });
+});
