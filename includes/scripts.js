be8e7aaa4de0d6134480536f394a852ec62a9c27
Amit Papushe Shely <papushe9@gmail.com>
2017-01-16 08:33:56 +0200

    last

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,21 +1,20 @@
-$(document).ready(function() {
+$("document").ready(function () {
 
-	// data: JSON.stringify({ "Name": userName, "Email" : email });
-	//
-	// $.ajax({
-	// url: "http://134.86.158.192/CodingTest/home/SignUp",
-	// type: "POST",
-	// data: {userNameData JSON.stringify(jsonObjects)},
-	// dataType: "json",
-	// beforeSend: function(x) {
-	// 	if (x && x.overrideMimeType) {
-	// 	x.overrideMimeType("application/j-son;charset=UTF-8");
-	// 	}
-	// },
-	// success: function(result) {
-	// 	import grails.converters.JSON;
-	// 	List userNameData = JSON.parse(params.userNameData);
-	// 	println "Hello " + userNameData[0].userName + ", you are signed up;)";
-	// }
-	// });
+	xhr = new XMLHttpRequest();
+	var url = "http://134.86.158.192/CodingTest/home/SignUp";
+	xhr.open("POST", url, true);
+	//xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function () { 
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var json = JSON.parse(xhr.responseText);
+			console.log(json.Name + ", " + json.Email);
+			$('form').append('<p>' + xhr.responseText + '</p>');
+		}
+		else{
+			console.log(xhr.responseText);
+			$('form').append('<p>' + xhr.responseText + '</p>');
+		}
+	}
+	var data = JSON.stringify({ "Name": "userName", "Email" : "email"});
+	xhr.send(data);
 });
